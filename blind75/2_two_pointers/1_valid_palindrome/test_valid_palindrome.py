acecc494c9830a0f4cbdd99e262c5e1421d20186
thehimel <thehimelofficial@gmail.com>
2023-05-29 00:09:47 +0200

    Updated 125. Valid Palindrome

--- blind75/2_two_pointers/1_valid_palindrome/test_valid_palindrome.py
+++ blind75/2_two_pointers/1_valid_palindrome/test_valid_palindrome.py
@@ -3,10 +3,12 @@ Author: Himel Das
 """
 
 import pytest
-from valid_palindrome import is_palindrome
+from valid_palindrome import is_palindrome, is_palindrome_oneliner
 
 
 @pytest.mark.parametrize("test_input, expected", [
     ("A man, a plan, a canal: Panama", True), ("race a car", False), (" ", True)])
 def test_contains_duplicate(test_input, expected):
     assert is_palindrome(test_input) == expected
+    assert is_palindrome_oneliner(test_input) == expected
+
--- blind75/2_two_pointers/1_valid_palindrome/valid_palindrome.py
+++ blind75/2_two_pointers/1_valid_palindrome/valid_palindrome.py
@@ -10,5 +10,18 @@ def is_palindrome(s: str) -> bool:
     TC: O(N)
     SC: O(N)
     """
+    sentence = ""
+    for char in s:
+        if char.isalnum():
+            sentence += char.lower()
+    return sentence == sentence[::-1]
+
+
+def is_palindrome_oneliner(s: str) -> bool:
+    """
+    Author: Himel Das
+    TC: O(N)
+    SC: O(N)
+    """
     s = "".join(filter(str.isalnum, s)).lower()
     return s == s[::-1]
