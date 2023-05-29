3ea81b144b9985c498f97ad6517800966bb33474
thehimel <thehimelofficial@gmail.com>
2023-05-29 22:54:26 +0200

    Added group_anagrams_optimized()

---
+++ blind75/1_arrays_and_hashing/4_group_anagrams/group_anagrams_optimized.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+
+def group_anagrams_optimized(strs: list[str]) -> list[list[str]]:
+    """
+    Author: Neetcode
+    TC: O(N)
+    SC: O(N)
+
+    Note: See group_anagrams_optimized.log
+    """
+    res = defaultdict(list)
+    for s in strs:
+        count = [0] * 26  # a ... z
+        for c in s:
+            count[ord(c) - ord("a")] += 1
+        res[tuple(count)].append(s)  # List can not be used as key in a dictionary but tuple can be for immutability.
+    return list(res.values())
