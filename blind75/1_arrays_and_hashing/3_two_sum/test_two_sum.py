011b30ce75f2701446b0b50f5dda5cb1da268692
thehimel <thehimelofficial@gmail.com>
2023-05-29 21:19:49 +0200

    Updated test case for two sum

--- blind75/1_arrays_and_hashing/3_two_sum/test_two_sum.py
+++ blind75/1_arrays_and_hashing/3_two_sum/test_two_sum.py
@@ -5,6 +5,6 @@ from two_sum import two_sum
 
 
 @pytest.mark.parametrize("test_input, expected", [
-    (([2, 7, 11, 15], 9), [0, 1]), (([3, 2, 4], 6), [1, 2]), (([3, 3], 6), [0,1])])
+    (([2, 7, 11, 15], 9), [0, 1]), (([3, 2, 4], 6), [1, 2]), (([3, 3], 6), [0, 1])])
 def test_contains_duplicate(test_input, expected):
     assert two_sum(*test_input) == expected
