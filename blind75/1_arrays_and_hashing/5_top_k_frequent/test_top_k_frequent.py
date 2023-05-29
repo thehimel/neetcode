e5bf5f4b34b7eca02832b151ecd9f23549122bfa
thehimel <thehimelofficial@gmail.com>
2023-05-30 00:51:47 +0200

    Added top_k_frequent_optimized

--- blind75/1_arrays_and_hashing/5_top_k_frequent/test_top_k_frequent.py
+++ blind75/1_arrays_and_hashing/5_top_k_frequent/test_top_k_frequent.py
@@ -1,10 +1,18 @@
-"""Author: Himel Das"""
+"""
+Author: Himel Das
+"""
 
 import pytest
 from top_k_frequent import top_k_frequent
+from top_k_frequent_optimized import top_k_frequent_optimized
 
 
 @pytest.mark.parametrize("test_input, expected", [
-    (([1, 1, 1, 2, 2, 3], 2), [1, 2]), (([1], 1), [1]), (([-1, -1], 1), [-1])])
+    (([1, 1, 1, 2, 2, 3], 2), [1, 2]),
+    (([1, 2, 3, 4, 5, 6], 2), [1, 2]),
+    (([2, 2, 2, 2, 2, 3, 4, 4, 6, 6], 3), [2, 4, 6]),
+    (([1], 1), [1]),
+    (([-1, -1], 1), [-1])])
 def test_contains_duplicate(test_input, expected):
     assert top_k_frequent(*test_input) == expected
+    assert top_k_frequent_optimized(*test_input) == expected
---
+++ blind75/1_arrays_and_hashing/5_top_k_frequent/top_k_frequent_optimized.py
@@ -0,0 +1,22 @@
+def top_k_frequent_optimized(nums: list[int], k: int) -> list[int]:
+    """
+    Author: Neetcode
+    Note: Solved with Bucket Sort.
+    SC: O(N)
+    TC: O(N)
+    """
+    count = {}
+    freq = [[] for _ in range(len(nums)+1)]
+
+    for n in nums:
+        count[n] = count.get(n, 0) + 1
+    for n, c in count.items():
+        # noinspection PyTypeChecker
+        freq[c].append(n)
+
+    res = []
+    for i in range(len(freq)-1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
