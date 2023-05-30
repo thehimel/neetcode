8ff28d1b2a8bb6753aeaf95c5e3dbfcf4b3618fe
thehimel <thehimelofficial@gmail.com>
2023-05-31 00:32:53 +0200

    Updated is_anagram_optimized()

--- blind75/1_arrays_and_hashing/2_valid_anagram/valid_anagram_optimized.py
+++ blind75/1_arrays_and_hashing/2_valid_anagram/valid_anagram_optimized.py
@@ -17,8 +17,8 @@ def is_anagram_optimized(s: str, t: str) -> bool:
     count_s, count_t = {}, {}
 
     for i in range(len(s)):
-        count_s[s[i]] = 1 + count_s.get(s[i], 0)
-        count_t[t[i]] = 1 + count_t.get(t[i], 0)
+        count_s[s[i]] = count_s.get(s[i], 0) + 1
+        count_t[t[i]] = count_t.get(t[i], 0) + 1
     for c in count_s:
         if count_s[c] != count_t.get(c, 0):
             return False
