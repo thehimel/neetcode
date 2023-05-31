c956d2f2f942196944c51e33abd0af439696a35f
thehimel <thehimelofficial@gmail.com>
2023-05-31 02:51:22 +0200

    Updated test case for 49. Group Anagrams

--- blind75/1_arrays_and_hashing/4_group_anagrams/test_group_anagrams.py
+++ blind75/1_arrays_and_hashing/4_group_anagrams/test_group_anagrams.py
@@ -9,6 +9,6 @@ from group_anagrams_optimized import group_anagrams_optimized
     (["eat", "tea", "tan", "ate", "nat", "bat"], [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]),
     ([""], [[""]]),
     (["a"], [["a"]])])
-def test_contains_duplicate(test_input, expected):
+def test_group_anagrams(test_input, expected):
     assert group_anagrams(test_input) == expected
     assert group_anagrams_optimized(test_input) == expected
