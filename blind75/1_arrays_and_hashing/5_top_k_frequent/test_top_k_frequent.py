7ad4cbcbb2c9f8ba7f39bf173ad95829b121569c
thehimel <thehimelofficial@gmail.com>
2023-05-31 02:50:57 +0200

    Updated test case for 347. Top K Frequent Elements

--- blind75/1_arrays_and_hashing/5_top_k_frequent/test_top_k_frequent.py
+++ blind75/1_arrays_and_hashing/5_top_k_frequent/test_top_k_frequent.py
@@ -13,6 +13,6 @@ from top_k_frequent_optimized import top_k_frequent_optimized
     (([2, 2, 2, 2, 2, 3, 4, 4, 6, 6], 3), [2, 4, 6]),
     (([1], 1), [1]),
     (([-1, -1], 1), [-1])])
-def test_contains_duplicate(test_input, expected):
+def test_top_k_frequent(test_input, expected):
     assert top_k_frequent(*test_input) == expected
     assert top_k_frequent_optimized(*test_input) == expected
