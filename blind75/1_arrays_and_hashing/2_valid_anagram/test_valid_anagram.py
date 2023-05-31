5f4381106b5c161c4099cd38dc680a799a2741f7
thehimel <thehimelofficial@gmail.com>
2023-05-31 02:52:03 +0200

    Updated test case for 242. Valid Anagram

--- blind75/1_arrays_and_hashing/2_valid_anagram/test_valid_anagram.py
+++ blind75/1_arrays_and_hashing/2_valid_anagram/test_valid_anagram.py
@@ -7,7 +7,7 @@ from valid_anagram_optimized import is_anagram_optimized
 
 @pytest.mark.parametrize("test_input, expected", [
     (("anagram", "nagaram"), True), (("rat", "car"), False), (("moon", "nom"), False)])
-def test_contains_duplicate(test_input, expected):
+def test_is_anagram(test_input, expected):
     assert is_anagram(*test_input) == expected
     assert is_anagram_sorted(*test_input) == expected
     assert is_anagram_optimized(*test_input) == expected
