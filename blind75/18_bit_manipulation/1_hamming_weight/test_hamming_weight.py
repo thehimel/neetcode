7999ef8ee15f239cd022b97bcc22d0404b8571db
thehimel <thehimelofficial@gmail.com>
2023-06-01 01:24:47 +0200

    Updated test_hamming_weight.py

--- blind75/18_bit_manipulation/1_hamming_weight/test_hamming_weight.py
+++ blind75/18_bit_manipulation/1_hamming_weight/test_hamming_weight.py
@@ -10,6 +10,6 @@ from hamming_weight import hamming_weight, hamming_weight_optimized
     (0o00000000000000000000000000001011, 3),
     (0o00000000000000000000000010000000, 1),
     (0o11111111111111111111111111111101, 31)])
-def test_is_palindrome(test_input, expected):
+def test_hamming_weight(test_input, expected):
     assert hamming_weight(test_input) == expected
     assert hamming_weight_optimized(test_input) == expected
