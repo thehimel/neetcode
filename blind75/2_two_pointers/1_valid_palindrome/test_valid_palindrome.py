cd541641b49eaf17dce051294b8e66ed27c0658a
thehimel <thehimelofficial@gmail.com>
2023-05-31 02:53:16 +0200

    Updated test case for 125. Valid Palindrome

--- blind75/2_two_pointers/1_valid_palindrome/test_valid_palindrome.py
+++ blind75/2_two_pointers/1_valid_palindrome/test_valid_palindrome.py
@@ -9,7 +9,7 @@ from valid_palindrome_optimized import is_palindrome_optimized
 
 @pytest.mark.parametrize("test_input, expected", [
     ("A man, a plan, a canal: Panama", True), ("race a car", False), (" ", True)])
-def test_contains_duplicate(test_input, expected):
+def test_is_palindrome(test_input, expected):
     assert is_palindrome(test_input) == expected
     assert is_palindrome_oneliner(test_input) == expected
     assert is_palindrome_optimized(test_input) == expected
